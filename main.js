fdd5920269ceeaafbffda491f93b2e1afe7cf4ec
duola110 <2979565338@qq.com>
2025-03-15 19:45:48 +0800

    tobbar完成

--- main.js
+++ main.js
@@ -2,6 +2,23 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import { $http } from '@escook/request-miniprogram'
+
+wx.$http = $http
+uni.$http = $http
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  wx.showLoading({
+    title: '数据加载中...',
+  })
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  wx.hideLoading()
+}
+
 
 Vue.config.productionTip = false
 
